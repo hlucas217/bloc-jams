f19b65843e77828606b3bf5eb09dcef85a79f43e
Heather Lucas <hlucas@gmail.com>
2016-12-31 15:54:43 -0500

    Complet assignment11-album-view

--- scripts/album.js
+++ scripts/album.js
@@ -8,8 +8,8 @@ var albumPicasso = {
          { title: 'Blue', duration: '4:26' },
          { title: 'Green', duration: '3:14' },
          { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
+         { title: 'Pink', duration: '3:21' },
+         { title: 'Magenta', duration: '2:15' }
      ]
  };
 
@@ -22,11 +22,26 @@ var albumMarconi = {
      songs: [
          { title: 'Hello, Operator?', duration: '1:01' },
          { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Fits in your pocket', duration: '3:21' },
          { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
+         { title: 'Wrong phone number', duration: '2:15' }
      ]
  };
+
+var albumNewsom = {
+    title: 'Ys', 
+    artist: 'Joanna Newsom', 
+    label: 'Drag City',
+    year: '2006', 
+    albumArtUrl: 'assets/images/album_covers/22.jpg',
+    songs: [
+        { title: 'Emily', duration: '12:07' },
+        { title: 'Monkey & Bear', duration: '9:29' },
+        { title: 'Sawdust & Diamonds', duration: '9:54' },
+        { title: 'Only Skin', duration: '16:53' },
+        { title: 'Cosmia', duration: '7:15' }
+    ]
+};
 
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
@@ -41,29 +56,41 @@ var createSongRow = function(songNumber, songName, songLength) {
     
 };
 
+// these are the elements that we want to populate with text dynamically
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+
 var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-     // #2
+     
+     // assign values to each part of the album (text, images)
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
  
-     // #3
+     // clear contents of album song list container
      albumSongList.innerHTML = '';
  
-     // #4
+     // build list of songs from album JavaScript object
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
 
 window.onload = function() {
-     setCurrentAlbum(albumPicasso);
+    setCurrentAlbum(albumPicasso);
+    
+    var albums = [albumPicasso, albumMarconi, albumNewsom];
+    var index = 1;
+    albumImage.addEventListener('click', function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == albums.length) {
+            index = 0;
+        }
+    });
  };
